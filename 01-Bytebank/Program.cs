64326f4301044c3e7a5144a20f19f5ed8f09a1b1
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2021-10-07 14:34:18 -0300

    C# POO

---
+++ 01-Bytebank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _01_Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 200;
+            
+
+            Console.WriteLine(contaDaGabriela.titular);
+            Console.WriteLine("Agência: " + contaDaGabriela.agencia);
+            Console.WriteLine("Número: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+            Console.WriteLine(resultadoSaque);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            bool resultadoDaTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+            
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+            Console.WriteLine("Resultado da Transferência: " + resultadoDaTransferencia);
+            Console.ReadLine();
+        }
+    }
+}
